e64fcd3469654220e506f33d7b217607d286e7f8
Analia <analiazunino@gmail.com>
2020-08-06 23:16:50 -0300

    actualice lista. h y lista.c y mate.c

--- list/list.c
+++ list/list.c
@@ -1,66 +1,106 @@
 #include "list.h"
-#include <stdlib.h>
 
-// completar
-void list_create(t_list *l){
-  *l = NULL;
+// Please: complete me
+
+list_t ls_create()
+{
+  return (list_t) NULL;
 }
 
-// ve si el elemento esta en la lista
-short list_find(t_list l, t_data e){
-  while(l)
-  {
-    if (l->data == e) return 1;
-    l = l->next;
+void ls_free(/*@inout@*/ list_t *self)
+{
+  // Please: complete me
+}
+
+int ls_to_file(FILE *fp, const /*@in@*/ list_t self)
+{
+  // Please: complete me
+  return -1;
+}
+
+bool ls_is_empty(const /*@in@*/ list_t self){
+   return self == NULL;
+}
+
+int ls_length(const /*@in@*/ list_t self){
+
+}
+
+int ls_includes(/*@in@*/ list_t self, const /*@in@*/ DATATYPE elem){
+
+}
+
+DATATYPE ls_head(const /*@in@*/ list_t self){
+  return header;
+}
+
+list_t ls_tail(const /*@in@*/ list_t self){
+  return tail;
+}
+
+/*
+ * ls_add_...
+ * 
+ * Return: 0: con 'exito 
+ *        -1: error (probablemente malloc)
+ */
+
+//error_t ls_add(/*@inout@*/ list_t *self, const /*@in@*/ DATATYPE elem);
+int ls_add(/*@inout@*/ list_t *self, const /*@in@*/ DATATYPE elem){
+  return ls_add_back(self, elem);
+}
+
+int ls_add_front(/*@inout@*/ list_t *self, const /*@in@*/ DATATYPE elem){
+  list_node_t* newNode = (list_node_t*)malloc(sizeof(list_node_t)); 
+  if (newNode == NULL) return 0;
+   
+  newNode->data = data;  
+  newNode->next = NULL;  
+  //chequea si la lista esta vacia
+  if(header == NULL) {  
+    header = newNode;  
+    tail = newNode;  
+    return 1;
+  }  
+  else { 
+    newNode->next = header; 
+    header = newNode;
+    return 1;
+  }  
+}
+
+
+int ls_add_back(/*@inout@*/ list_t *self, const /*@in@*/ DATATYPE elem){
+  list_node_t* newNode = (list_node_t*)malloc(sizeof(list_node_t)); 
+  if (newNode == NULL) return 0;
+   
+  newNode->data = data;  
+  newNode->next = NULL;  
+  //chequea si la lista esta vacia
+  if(header == NULL) {  
+    header = newNode;  
+    tail = newNode;  
+    return 1;
+  }  
+  else { 
+    tail->next = newNode;
+    tail = newNode;
+    return 1;
   }
-  return 0;
 }
 
-short list_is_empty(const t_list l){
-  return l == NULL;
-}
+/* Precond: !ls_empty(self) */
+DATATYPE ls_remove(/*@inout@*/ list_t *self);
 
-// fijarse que devuelve un floatt deberia ser un error ponele
-t_data list_add(t_list *l, t_data e){
-  t_list_node *node = malloc(sizeof(t_list_node));
-  node->data = e;
-  node->next = NULL;
-  if (*l == NULL) *l = node;
-  else {
-    t_list_node *n = *l;
-    while (n->next) n = n->next;
-    n->next = node;
-  }
-  return e;
-}
+/*
+ * status code:
+ * 0: OK operaci'on con 'exito
+ * -1: error, lista vac'ia
+ * En el caso de status=-1 el valor del par'ametro 
+ * elem debe ser ignroado
+ */
+void ls_remove_status(/*@inout@*/ list_t *self, 
+                      /*@out@*/   DATATYPE *elem, 
+                      /*@out@*/   int *status){
 
-t_data  list_delete(t_list *l, t_data e){
-  if (list_find(*l,e))
-  {
-    t_list_node *prev = NULL, *node = *l;
-    while(node->data != e && node)
-    {
-      prev = node;
-      node = node->next;
-    }
-    if (!prev) *l = node->next;
-    else
-    {
-      prev->next = node->next;
-    }
-    free(node);
-  }
-  return e;
-}
-
-/* Libera las estructuras internas usadas por este tipo de dato */
-void list_free(t_list *l) {
-t_list_node *node = *l;
-  while(node)
-  {t_list_node *aux;
-    aux = node;
-    node = node->next;
-    free(aux);
-  }
-  *l=NULL;
-}
+                      }
